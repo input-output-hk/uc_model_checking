1b820da2fa3feac3dde97a1091431055c8c77fe4
Denis Firsov <denis.firsov@gmail.com>
2026-04-26 19:40:37 +0300

    fixes and params

--- run_simulations.sh
+++ run_simulations.sh
@@ -85,12 +85,11 @@ run_must_fails() {
 
 # ====== Safety simulations (must hold) ======
 
-run_sim "f_crs"    "$SPECS_DIR/f_crs/f_crs_properties.qnt"       "f_crs_properties"
+
 run_sim "f_hash"   "$SPECS_DIR/f_hash/f_hash_properties.qnt"     "f_hash_properties"
 run_sim "f_sig"    "$SPECS_DIR/f_sig/f_sig_properties.qnt"       "f_sig_properties"
 run_sim "f_dif"    "$SPECS_DIR/f_dif/f_dif_properties.qnt"       "f_dif_properties"
 run_sim "f_inizk"  "$SPECS_DIR/f_inizk/f_inizk_properties.qnt"   "f_inizk_properties"
-run_sim "f_ac"     "$SPECS_DIR/f_ac/f_ac_properties.qnt"         "f_ac_properties"
 run_sim "f_atms"   "$SPECS_DIR/f_atms/f_atms_properties.qnt"     "f_atms_properties"
 run_sim "f_pki"    "$SPECS_DIR/f_pki/f_pki_properties.qnt"       "f_pki_properties"
 run_sim "g_clock"  "$SPECS_DIR/g_clock/g_clock_properties.qnt"   "g_clock_properties"
@@ -99,8 +98,6 @@ run_sim "p_bridge" "$SPECS_DIR/p_bridge/p_bridge_properties.qnt" "p_bridge_prope
 
 # ====== Coverage checks (must fail) ======
 
-run_must_fails "f_crs" "$SPECS_DIR/f_crs/f_crs_properties.qnt" "f_crs_properties" \
-    "CRS_different"
 
 run_must_fails "f_hash" "$SPECS_DIR/f_hash/f_hash_properties.qnt" "f_hash_properties" \
     "HashLiveness"
